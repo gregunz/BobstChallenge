a0a0ac8268440ccafaba13d97c10bd78722b516e
gr3g <gregoire.clement@epfl.ch>
2017-11-12 01:41:23 +0100

    gregunz's code

--- impl.py
+++ impl.py
@@ -3,16 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def roi(img):
+def contour(img):
     
     _, thresh = cv2.threshold(img,127,255,0)
     
     _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnt = max(contours, key = cv2.contourArea)
     
-    area = cv2.contourArea(cnt)
-    
-    return area
+    return cnt
 
 def crop(img, area):
     
@@ -54,5 +52,19 @@ def get_min_width_height(images):
 
 def show(img):
     plt.figure(figsize=(20, 20))
-    plt.imshow(img, cmap='gray')
+    plt.imshow(img, cmap='gray', vmin = 0, vmax = 255)
     plt.show()
+    
+def mask_contour(img, perc=0.98):
+    cnt = contour(img)
+    full_mask = np.zeros(template.shape, dtype=np.uint8)
+    tmp1 = cv2.drawContours(full_mask, [cnt], 0, 255, -1)
+    w, h = tmp1.shape
+    tmp2 = cv2.resize(tmp1, (int(h * perc), int(w * perc)), interpolation=cv2.INTER_AREA)
+    tmp3 = np.zeros_like(tmp1)
+    smaller_w, smaller_h = tmp2.shape
+    w_dif = w - smaller_w
+    h_dif = h - smaller_h
+    tmp3[w_dif // 2:smaller_w + w_dif // 2, h_dif // 2:smaller_h + h_dif // 2] = tmp2
+    mask = tmp3 // 128
+    return img * mask
